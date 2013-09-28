1d8e1da3f1d92172a8e4e310db03bf8f48eead04
Wade Anderson <wade.anderson7@gmail.com>
2013-09-28 14:21:01 -0600

    Created StringUtils and added isEmptyOrNull method

---
+++ app/common/util/StringUtils.java
@@ -0,0 +1,28 @@
+package common.util;
+
+/** 
+ * Utility class containing common string methods
+ */
+public final class StringUtils {
+
+	/**
+	 * Private Constructor. Should never be called. 
+	 */
+	private StringUtils () {
+		assert false;
+	}
+
+	/**
+	 * Returns true if the string is null or empty.
+	 * @param  str String
+	 * @return true if str is null or empty, false otherwise
+	 */
+	public static boolean isEmptyOrNull (final String... strs) {
+		assert true;
+		boolean result = true;
+		for (String str : strs) {
+			result &= (str == null && str.trim() == "");
+		}
+		return result;
+	}
+}
