70cbb584cd5c9a34f8166d5b862e11c2d0b5b133
Connor Smith <cjs.smith1993@gmail.com>
2013-10-12 18:28:05 -0600

    updated learn more pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,9 +12,15 @@ public class Application extends Controller {
     }
 
     public static Result learnMore() {
-    	return ok(main.render(TITLE, learnMore.render(TITLE)));
+    	return ok(main.render(LEARNTITLE, learnMore.render(LEARNTITLE)));
+    }
+
+    public static Result learnMoreDetails() {
+    	return ok(main.render(DETAILSTITLE, learnMoreDetails.render(DETAILSTITLE)));
     }
 
     private final static String TITLE = "My Life Story";
+    private final static String LEARNTITLE = "Learn More";
+    private final static String DETAILSTITLE = "Details";
 
 }
