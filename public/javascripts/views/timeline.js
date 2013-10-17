24e08d0a063490f72d14d54d55025b1fae6e8508
Mike Brodie <mike1bass@aol.com>
2013-10-16 22:52:32 -0600

    added css scaling transformation

--- public/javascripts/views/timeline.js
+++ public/javascripts/views/timeline.js
@@ -1,5 +1,13 @@
-//$(function () {
+
 	$(document).ready(function() {
-		$('#carousel-demo1').scrollingCarousel();			
+		$('#carousel-demo1').scrollingCarousel();	
+
+		$('.timelineItem').click(function(e) {
+			$('.timelineItem').css('border', 'none');
+	  		$(e.target).css('border', '5px solid black');
+	  		$('.enlargedTimelineItem').removeClass('enlargedTimelineItem');		
+	  		$(e.target).addClass('enlargedTimelineItem');
+	});		
 	});
-//}
+
+	
