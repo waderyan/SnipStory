789ffb9008750269d50867a68ae7dc5d42a1fe0a
Steve525 <clarksones@gmail.com>
2013-11-04 14:48:24 -0700

    Cleared up code and now this uses tablesorter, a jquery plugin for sorting tables on the client side. Also, hitting refresh DOES NOT duplicate table information now. Fixed that problem by updating outside of the AJAX call.

--- public/javascripts/admin/admin.js
+++ public/javascripts/admin/admin.js
@@ -1,18 +1,15 @@
 $(function () {
 
 	$('#refresh-invitees').click(function() {
-		var newTable = $("<tbody id='invitee-table'>");
-		var totalInvitees = $("#total-invitees");
-		totalInvitees.empty();
-		var pages = $("#show-pages");
-		pages.empty();
+		var table = $("#invitee-table");
+		table.empty();
 
 		function populateTable (tableItems) {
 			for (var i = 0; i < tableItems.length; i++) {
 				var email = tableItems[i].email;
 				var name = tableItems[i].name;
 				var createdAt = tableItems[i].createdat;
-				newTable.append(
+				table.append(
 					'<tr>' +
 						'<td>' + (i+1) + '</td>' +
 						'<td>' + name + '</td>' +
@@ -21,10 +18,6 @@ $(function () {
 					'</tr>'
 				);
 			}
-			totalInvitees.append("(" + tableItems.length + ")");
-			var oldTable = $("#invitee-table");
-			// ****  Replace at the end to prevent flickering on refresh  *********
-			oldTable.replaceWith(newTable);
 		}
 
 		$.ajax({
@@ -33,20 +26,23 @@ $(function () {
 			url: 'http://' + document.location.host + '/admin/invitees',
 			success: function (data) {
 				populateTable(data);
+				$("#invitees-table").tablesorter( {sortList: [[3,1]], widgets: ['zebra']} );
 			},
 			error: function(xhr) {
 				console.log("ERROR!");
 				console.dir(xhr);
 			}
 		});
+		$("#invitees-table").trigger("update");	// don't reinitialize tablesorter again, just update
 	});
 
 	$('#refresh-feedback').click(function() {
-		var newTable = $("<tbody id='feedback-table'>");
+		var table = $("#feedback-table");
+		table.empty();
 
 		function populateTable (tableItems) {
 			tableItems.forEach(function (item, i) {
-				newTable.append(
+				table.append(
 					'<tr>' +
 						'<td>' + (i+1) + '</td>' +
 						'<td>' + item.feature + '</td>' +
@@ -55,9 +51,6 @@ $(function () {
 					'</tr>'
 				);
 			});
-			var oldTable = $("#feedback-table");
-			// ****  Replace only at the end to prevent flickering on refresh  *********
-			oldTable.replaceWith(newTable);
 		}
 
 		$.ajax({
@@ -66,161 +59,20 @@ $(function () {
 			url: 'http://' + document.location.host + '/admin/feedback',
 			success: function (data) {
 				populateTable(data);
+				$("#feedbacks-table").tablesorter( {sortList: [[1,0]], widgets: ['zebra']} );
 			},
 			error: function(xhr) {
 				console.log("ERROR!");
 				console.dir(xhr);
 			}
 		});
+		$("#feedbacks-table").trigger("update");
 	});
 
-	$('#sort-a').click(function() {
-		var newTable = $("<tbody id='invitee-table'>");
-
-		function populateTable (tableItems) {
-			for (var i = 0; i < tableItems.length; i++) {
-				var email = tableItems[i].email;
-				var name = tableItems[i].name;
-				var createdAt = tableItems[i].createdat;
-				newTable.append(
-					'<tr>' +
-						'<td>' + (i+1) + '</td>' +
-						'<td>' + name + '</td>' +
-						'<td><a href="mailto:'+ email + '">' + email + '</a></td>' +
-						'<td>' + createdAt + '</td>' +
-					'</tr>'
-				);
-			}
-			var oldTable = $("#invitee-table");
-			// ****  Replace at the end to prevent flickering on refresh  *********
-			oldTable.replaceWith(newTable);
-		}
-
-		$.ajax({
-			type: 'GET',
-			dataType: "json",
-			url: 'http://' + document.location.host + '/admin/invitees/sortNameA',
-			success: function (data) {
-				populateTable(data);
-			},
-			error: function(xhr) {
-				console.log("ERROR!");
-				console.dir(xhr);
-			}
-		});
-
-	});
-
-	$('#sort-z').click(function() {
-		var newTable = $("<tbody id='invitee-table'>");
-
-		function populateTable (tableItems) {
-			for (var i = 0; i < tableItems.length; i++) {
-				var email = tableItems[i].email;
-				var name = tableItems[i].name;
-				var createdAt = tableItems[i].createdat;
-				newTable.append(
-					'<tr>' +
-						'<td>' + (i+1) + '</td>' +
-						'<td>' + name + '</td>' +
-						'<td><a href="mailto:'+ email + '">' + email + '</a></td>' +
-						'<td>' + createdAt + '</td>' +
-					'</tr>'
-				);
-			}
-			var oldTable = $("#invitee-table");
-			oldTable.replaceWith(newTable);
-		}
-
-		$.ajax({
-			type: 'GET',
-			dataType: "json",
-			url: 'http://' + document.location.host + '/admin/invitees/sortNameZ',
-			success: function (data) {
-				populateTable(data);
-			},
-			error: function(xhr) {
-				console.log("ERROR!");
-				console.dir(xhr);
-			}
-		});
-
-	});
-
-	$('#sort-recent').click(function() {
-		var newTable = $("<tbody id='invitee-table'>");
-
-		function populateTable (tableItems) {
-			for (var i = 0; i < tableItems.length; i++) {
-				var email = tableItems[i].email;
-				var name = tableItems[i].name;
-				var createdAt = tableItems[i].createdat;
-				newTable.append(
-					'<tr>' +
-						'<td>' + (i+1) + '</td>' +
-						'<td>' + name + '</td>' +
-						'<td><a href="mailto:'+ email + '">' + email + '</a></td>' +
-						'<td>' + createdAt + '</td>' +
-					'</tr>'
-				);
-			}
-			var oldTable = $("#invitee-table");
-			oldTable.replaceWith(newTable);
-		}
-
-		$.ajax({
-			type: 'GET',
-			dataType: "json",
-			url: 'http://' + document.location.host + '/admin/invitees/sortRecent',
-			success: function (data) {
-				populateTable(data);
-			},
-			error: function(xhr) {
-				console.log("ERROR!");
-				console.dir(xhr);
-			}
-		});
-
-	});
-
-	$('#sort-first').click(function() {
-		var newTable = $("<tbody id='invitee-table'>");
-
-		function populateTable (tableItems) {
-			for (var i = 0; i < tableItems.length; i++) {
-				var email = tableItems[i].email;
-				var name = tableItems[i].name;
-				var createdAt = tableItems[i].createdat;
-				newTable.append(
-					'<tr>' +
-						'<td>' + (i+1) + '</td>' +
-						'<td>' + name + '</td>' +
-						'<td><a href="mailto:'+ email + '">' + email + '</a></td>' +
-						'<td>' + createdAt + '</td>' +
-					'</tr>'
-				);
-			}
-			var oldTable = $("#invitee-table");
-			oldTable.replaceWith(newTable);
-		}
-
-		$.ajax({
-			type: 'GET',
-			dataType: "json",
-			url: 'http://' + document.location.host + '/admin/invitees/sortFirst',
-			success: function (data) {
-				populateTable(data);
-			},
-			error: function(xhr) {
-				console.log("ERROR!");
-				console.dir(xhr);
-			}
-		});
-
-	});
 
 	// Populate tables on page load.
 	$('#refresh-feedback').click();
 	$('#refresh-invitees').click();
+
 
 });
